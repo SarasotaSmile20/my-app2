7ca85f51c6f2c7e5b2ba33c31e7100ba898cef60
SarasotaSmile20 <sarasotasmile20@outlook.com>
2025-04-13 00:47:49 -0400

    update

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './EmployeeList.css';
+import { Link } from 'react-router-dom';
 
-function EmployeeList({ employees, editEmployee, removeEmployee }) {
+function EmployeeList({ employees, onEdit, removeEmployee }) {
   return (
     <div className="employee-list">
       <h3>Employee List</h3>
       <ul>
-        {employees.map((employee, index) => (
-          <li key={index}>
-            <div className="employee-details">
-              {employee.name} - {employee.email} - {employee.title} - {employee.department}
-            </div>
+        {employees.map((employee) => (
+          <li key={employee.id}>
+            <Link to={`/employee/${employee.id}`} className="employee-name">
+              {employee.name}
+            </Link>
             <div className="employee-actions">
-              <button onClick={() => editEmployee(index)}>Edit</button>
-              <button onClick={() => removeEmployee(index)}>Remove</button>
+              <button className="edit-btn" onClick={() => onEdit(employee)}>Edit</button>
+              <button className="remove-btn" onClick={() => removeEmployee(employee.id)}>Remove</button>
             </div>
           </li>
         ))}
